56b8234156059885c8243e4ab29c0f33145f1165
magwindow <antosha.petrakov@yandex.ru>
2025-11-09 20:58:30 +0300

    tcp server

---
+++ main.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+def scan(target_host, target_port):
+    # create a socket object
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect the client
+    client.connect((target_host, target_port))
+    # send some data
+    client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+    # receive some data
+    response = client.recv(4096)
+    return response
+
+# pprint(scan(target_host, target_port))
+
+
